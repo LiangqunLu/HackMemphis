8e365a532049b4a2763d4ea444fc29a21f88a10e
Unknown <liangqunlu@gmail.com>
2017-09-09 17:05:09 -0500

    add login in app

--- login/views.py
+++ login/views.py
@@ -6,4 +6,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, this is a housing information page")
+    #return HttpResponse("Hello, this is a housing information page")
+
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'index.html', context)
+
+
